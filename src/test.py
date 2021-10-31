44ed59bfc9200891249587dc3942a6c31a7c9ba0
TOMOKI O <64746406+tomokio@users.noreply.github.com>
2021-10-31 21:00:45 +0900

    Added test.py.

---
+++ src/test.py
@@ -0,0 +1,30 @@
+import os
+from   utils           import load_model
+from   config          import load_config
+from   dataset         import load_test_set
+from   sklearn.metrics import classification_report
+
+
+def _calc_score(test_set):
+    y_true = [sample.y_true for sample in test_set]
+    y_pred = [sample.y_pred for sample in test_set]
+    print(classification_report(y_true, y_pred, target_names=['-1', '1']))
+
+def test(config):
+    model_path    = config.MODEL_DIR + os.sep + 'svm_trained_model.pkl'
+    trained_model = load_model(model_path)
+
+    test_set      = load_test_set(config)
+    test_set_X    = [sample.X for sample in test_set]
+
+    pred_list     = trained_model.predict(test_set_X)
+    for sample_i, sample in enumerate(test_set):
+        sample.y_pred = pred_list[sample_i]
+
+    _calc_score(test_set)
+
+
+if __name__ == '__main__':
+
+    config  = load_config()
+    test(config)
